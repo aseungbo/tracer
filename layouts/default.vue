ccca52fb49235ef8483db57021b4aa381279e8dc
aseungbo <seungbo1112@gmail.com>
2023-07-14 17:20:40 +0900

    feat: Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,11 +6,61 @@
           <NuxtPage />
         </VContainer>
       </VMain>
+      <footer
+        class="w-100 d-flex flex-column justify-center align-center"
+        style="background-color: #191f28; color: #fff; padding: 100px 0"
+      >
+        <div class="mb-4">
+          <p class="text-subtitle-2 text-sm-subtitle-1" style="color: #b0b8c1; font-weight: 500">
+            Contact
+          </p>
+        </div>
+        <div>
+          <p class="text-subtitle-2 text-sm-subtitle-1 mb-8 text-center" style="color: #6b7684">
+            이메일 : contact@tracer.team
+          </p>
+          <ul class="d-flex justify-center" style="list-style: none; gap: 3rem">
+            <li>
+              <a
+                style="cursor: pointer; border-radius: 50%; opacity: 0.7"
+                href="https://www.threads.net/@tracer_company"
+              >
+                <v-img width="36" style="color: #fff" :src="threads"></v-img>
+              </a>
+            </li>
+            <li>
+              <a
+                style="cursor: pointer; border-radius: 50%; opacity: 0.7"
+                href="https://blog.naver.com/tracer42/223154991451"
+              >
+                <v-icon class="w-100 h-100" size="36" icon="mdi-alpha-n-box-outline"></v-icon>
+              </a>
+            </li>
+            <li>
+              <a
+                style="cursor: pointer; border-radius: 50%; opacity: 0.7"
+                href="https://www.youtube.com/watch?v=YiiD9ddqbOc"
+                ><v-icon size="36" icon="mdi-youtube"></v-icon>
+              </a>
+            </li>
+            <li>
+              <a
+                style="cursor: pointer; border-radius: 50%; opacity: 0.7"
+                href="https://www.instagram.com/tracer_company"
+              >
+                <v-icon size="36" icon="mdi-instagram"></v-icon>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </VApp>
   </div>
 </template>
 
 <script setup>
+import threads from "@/assets/threads.svg";
+
 const pageHeight = ref("");
 
 onMounted(() => {
